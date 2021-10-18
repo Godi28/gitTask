ccd0e20307b60e8688eb63939341714aa30201d5
Godi <gsehoole1@gmail.com>
2021-10-18 22:10:04 +0200

    program asking for user input and printing it

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
-alert("Git is Awesome!");
+let body = document.getElementById("body");
+let input = document.createElement("input");
+let button = document.createElement("button");
+body.appendChild(input);
+body.appendChild(button);
+button.innerHTML = "submit"
+button.onclick = function () {
+  body.innerHTML = input.value;
+};
